4ac277c44b1371786747b85629c199c2c703b4ad
leomonu <pratheerthk@gmail.com>
2020-10-23 19:46:32 -0700

    Dictionary Offline

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
+import db from './config';
+import {Header} from 'react-native-elements';
+import dictionary from './localdb';
+export default class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      word:'',
+      lexicalCategory:'',
+      definition:'',
+      isSearchedPressed:false,
+      text:'',
+      
+    }
+  }
+  getWord= (t)=>{
+    var text = t.toLowerCase();
+     const word = dictionary[text]['word'];
+    const lexicalCategory = dictionary[text]['lexicalCategory'];
+    const definition = dictionary[text]['definition'];
+      this.setState({
+        word:word,
+        lexicalCategory:lexicalCategory,
+        definition:definition     
+       })
+    }
+    
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  render(){
+   return (
+    <View>
+      <Header
+          backgroundColor={'yellow'}
+          centerComponent={{
+            text: 'Pocket Dictionary',
+            style: { color: 'blue', fontSize: 20, fontWeight: 'bold' },
+          }}
+        />
+        <TextInput 
+           style = {styles.inputBox}
+           onChangeText = {text=>{
+             this.setState({
+               text:text,
+               
+               word:'Loading...',
+               lexicalCategory:'',
+               
+               definition:'',
+
+             })
+           }}     
+           value = {this.state.text}
+        />
+      <TouchableOpacity
+      style = {styles.searchButton}
+      onPress = {()=>{this.getWord(this.state.text)}
+        
+      }
+      >
+        <Text> Search</Text>
+      </TouchableOpacity>
+      <View>
+      <Text>
+        word:{this.state.word}
+        
+      </Text>
+      <Text>
+      lexicalCategory:{this.state.lexicalCategory}
+      </Text>
+      <Text>
+        Definition:{this.state.definition}
+      </Text>
+      
+      </View>
+      
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  inputBox: {
+    marginTop: 60,
+    width: '80%',
+    borderWidth: 4,
+    alignSelf: 'center',
+    textAlign: 'center',
+    outline: 'none',
+    height: 40,
   },
-});
+  searchButton:{
+    width:100,
+    height:100,
+    backgroundColor:'blue',
+    
+  }
+  
+})
+
+
